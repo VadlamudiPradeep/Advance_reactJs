a9e8fba35f85544c8a0e7388d7a6e6e26994e166
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-07-26 20:20:14 +0530

    creating_the_your_own_first_component

---
+++ Components/NewExpenses/ExpenseForm.js
@@ -0,0 +1,46 @@
+
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+
+    const titleChange = (event)=>{
+        event.preventDefault();
+        console.log(event.target.value)
+        console.log('Titl Updated')
+    }
+
+    const amountChange = (event)=>{
+        event.preventDefault();
+        console.log('Amount Updated')
+    };
+  
+    const dateChange = (event)=>{
+        event.preventDefault();
+        console.log('Date Updated')
+    }
+
+
+  return (
+    <form>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input type='text' onChange={titleChange}/>
+        </div>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input type='number' min='0.01' step='0.01' onChange={amountChange}/>
+        </div>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input type='date' min='2019-01-01' max='2022-12-31'onChange={dateChange} />
+        </div>
+      </div>
+      <div className='new-expense__actions'>
+        <button type='submit'>Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
