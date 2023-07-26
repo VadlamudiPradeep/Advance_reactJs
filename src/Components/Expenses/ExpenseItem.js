20eac262eadc9f9bf4e4b0da8be4bf451d22f172
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-07-26 20:07:33 +0530

    Createing_your_own_first_Component

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,40 +1,29 @@
-
 import React, { useState } from 'react';
 
-import ExpenseDate  from './ExpenseDate.js'
-import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-
-
-let ExpenseItem = (props)=>{
-   const [title , setTitle] =  useState(props.title);
-//let title =  props.title;
-const [amount , setAmount]  = useState(props.amount)
-    var clickHandler = ()=>{
-        console.log('Deleting expenses: ' , title)
-    setTitle('Deleted');
-
-      };
-  const ClickHandlerAddExpenses=()=>{
-    setAmount('$100')
-  }
+const ExpenseItem = (props) => {
+  // function clickHandler() {}
+  const [title, setTitle] = useState(props.title);
   
-    return (
-        <div>
-        <Card className='expense-item'>
-    <ExpenseDate date={props.date} />
-    <div className='expense-item__description'>
-      <p>Location: {props.locationOfExpenditure}</p>
-      <h2>{title}</h2>
-      <div className="expense-item__price">{amount}</div>
-    </div>
-    <button onClick={clickHandler}>Delete Expense</button>
-    <button onClick={ClickHandlerAddExpenses}>Add Expenses</button>
-  </Card>
-  </div>
+  
+  const clickHandler = () => {
+    setTitle('Updated!');
+ 
+  };
 
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
 
-    )
-};
-export default ExpenseItem
+export default ExpenseItem;
